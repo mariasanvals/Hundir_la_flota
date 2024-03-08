d6dc7d865d20d6cc9a5b36ca5450bc1d9869048a
aritzcerezo <aritz.priv@gmail.com>
2024-03-08 19:58:17 +0100

    erratas ultima hora

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ![image](images/image.png)
 Programa que simula el famoso juego de hundir la flota.
 El jugador vs la maquina.
-El programa se in inicializa posicionando los barcos de ambos jugadores aleatoriamente.
+El programa se inicializa posicionando los barcos de ambos jugadores aleatoriamente.
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 Programa que simula el famoso juego de hundir la flota.
 El jugador vs la maquina.
-El programa se in inicializa posicionando los barcos de ambos jugadores aleatoriamente
+El programa se inicializa posicionando los barcos de ambos jugadores aleatoriamente
 '''
 
 # Importamos librerias
